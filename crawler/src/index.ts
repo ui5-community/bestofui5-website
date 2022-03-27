dfeea0bc81cbc5e94892577b731ddfd016fff2a4
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2022-03-27 14:29:22 +0200

    Refactor Crawler to read from repos from json file (#22)
    * merge
    
    * feat(crawler): refactor to read repos from json file
    
    * fix github token

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -2,7 +2,7 @@
 // import { readFileSync, writeFileSync } from "fs";
 
 import GitHubRepositoriesProvider from "./gh-repos";
-import { Artifact } from "./types";
+import { Artifact, Package } from "./types";
 
 (async () => {
   // get current month data
@@ -31,55 +31,10 @@ import { Artifact } from "./types";
   const artifacts = await Promise.all(
     Providers.map(async (provider) => {
       // console.log(`Start provider '${provider.name}'.`);
-      const items: Artifact[] = await provider.get(currentTrendsJson);
+      const items: Package[] = await provider.get(currentTrendsJson);
       // console.log(`Provider '${provider.name}' returned ${items.length} items.`);
       return items;
     })
   );
 
-  // const artifactsMap: any = {};
-  // artifacts.flat().forEach((artifact: any) => {
-  //   artifactsMap[artifact.id] = artifact;
-  // });
-
-  // //loop over new data and update current data
-  // for (let key in artifactsMap) {
-  //   let newData = artifactsMap[key];
-  //   // find single object in dict array
-  //   currentTrendsJson.newlyAdded.forEach((item: any) => {
-  //     if (item.id === newData.id) {
-  //       item.description = newData.description;
-  //       item.tags = newData.tags;
-  //       item.updatedAt = newData.updatedAt;
-  //     }
-  //   });
-  //   currentTrendsJson.overall.forEach((item: any) => {
-  //     if (item.id === newData.id) {
-  //       item.description = newData.description;
-  //       item.tags = newData.tags;
-  //       item.updatedAt = newData.updatedAt;
-  //     }
-  //   });
-  //   currentTrendsJson.recentlyUpdated.forEach((item: any) => {
-  //     if (item.id === newData.id) {
-  //       item.description = newData.description;
-  //       item.tags = newData.tags;
-  //       item.updatedAt = newData.updatedAt;
-  //     }
-  //   });
-  //   currentAllItemsJson.forEach((item: any) => {
-  //     if (item.id === newData.id) {
-  //       item.description = newData.description;
-  //       item.tags = newData.tags;
-  //       item.updatedAt = newData.updatedAt;
-  //     }
-  //   });
-  // }
-
-  // // write new data to github pages folder
-  // writeFileSync(`${__dirname}/../../docs/model/allItems.json`, JSON.stringify(currentAllItemsJson));
-  // writeFileSync(`${__dirname}/../../docs/model/trends.json`, JSON.stringify(currentTrendsJson));
-  // // write new data to frontend data
-  // writeFileSync(`${__dirname}/../../frontend/trends/webapp/model/allItems.json`, JSON.stringify(currentAllItemsJson));
-  // writeFileSync(`${__dirname}/../../frontend/trends/webapp/model/trends.json`, JSON.stringify(currentTrendsJson));
 })();
