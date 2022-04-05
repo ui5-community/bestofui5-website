5e494950add2aa1eb21ab7b6e12289af5d86740a
Peter Muessig <peter.muessig@sap.com>
2022-04-05 15:22:33 +0200

    build: switch to mono repo

---
+++ packages/ui/src/controller/App.controller.ts
@@ -0,0 +1,13 @@
+import BaseController from "./BaseController";
+
+/**
+ * @namespace org.openui5.ui5community.controller
+ */
+export default class App extends BaseController {
+
+	public onInit() : void {
+		// apply content density mode to root view
+		this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+	}
+
+}
