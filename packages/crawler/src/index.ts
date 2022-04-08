96385da62568ee1827b203778cd62eea2f2051a1
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-04-08 18:58:54 +0200

    feat(crawler): get tags and types from packages json

--- packages/crawler/src/index.ts
+++ packages/crawler/src/index.ts
@@ -1,35 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 // require("dotenv").config();
 import { readFileSync, writeFileSync } from "fs";
 
 import GitHubRepositoriesProvider from "./gh-repos";
 import NPMProvider from "./npm";
-import { Artifact, Package, Source, Type, DataJson } from "./types";
+import { Package, Source, Tags, Types, DataJson } from "./types";
 
 (async () => {
-  let dataJson: DataJson = {
+  const dataJson: DataJson = {
     types: [],
     packages: [],
+    tags: []
   };
 
   const sourcesJsonString = readFileSync(`${__dirname}/../sources.json`, "utf8");
-  let sources: Source[] = JSON.parse(sourcesJsonString);
+  const sources: Source[] = JSON.parse(sourcesJsonString);
 
   let githubPackages: Package[] = await GitHubRepositoriesProvider.get(sources);
   githubPackages = await NPMProvider.get(githubPackages);
 
-  // extract type from packages info
-  let typesArray: Type[] = [];
+  // extract tags from packages info
+  const typesArray: Tags[] = [];
+  const tagsArray: Types[] = [];
   for (const packageContent of githubPackages) {
-    let type: Type = {
-      name: packageContent.type,
-    };
-    if (!typesArray.find((type) => type.name === packageContent.type)) {
-      typesArray.push(type);
+    
+    for (const type of packageContent["ui5-community"].types) {
+      if (!typesArray.find((tag) => tag.name === type)) {
+        const tags: Tags = {
+          name: "",
+        };
+        tags.name = type;
+        typesArray.push(tags);
+      }
+    }
+    for (const tag of packageContent["ui5-community"].tags) {
+      if (!tagsArray.find((tagArray) => tagArray.name === tag)) {
+        const tags: Tags = {
+          name: "",
+        };
+        tags.name = tag;
+        tagsArray.push(tags);
+      }
     }
   }
 
   dataJson.packages = githubPackages;
   dataJson.types = typesArray;
+  // for now combine them
+  dataJson.tags = tagsArray.concat(typesArray);
+
 
   writeFileSync(`${__dirname}/../../ui/src/model/data.json`, JSON.stringify(dataJson));
 
