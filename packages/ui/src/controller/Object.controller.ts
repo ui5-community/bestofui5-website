b449d51ff9c9192e1b25b53072aaa7143641b782
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2022-04-13 17:23:22 +0200

    fix(ui): fix on "await on jsonmodel loaded" (#102)
    * fix(ui): await on jsonmodel loaded
    
    * chore: add docs to prettierignore
    
    * fix: change build commit message

--- packages/ui/src/controller/Object.controller.ts
+++ packages/ui/src/controller/Object.controller.ts
@@ -10,22 +10,23 @@ export default class Main extends BaseController {
 		this.getRouter().getRoute("RouteObjectView").attachPatternMatched(this.onPatternMatched, this);
 	}
 
-  public async onPatternMatched(event): Promise<void> {
-    const objectName = event.getParameter("arguments").name;
-    const model = await this.getModelAwait("data");
-    const data = model.getData();
-    // find object index in data
-    const objectIndex = data.packages.findIndex((object) => object.name === objectName);
-    if (!objectIndex) {
-      //object not found
-      // return
-    }
-    this.getView().bindElement({
-      path: `/packages/${objectIndex}`,
-      model: "data",
-    });
-  }
-  
+	public async onPatternMatched(event): Promise<void> {
+		const objectName = event.getParameter("arguments").name;
+		const model = this.getModel("data");
+		await model.dataLoaded();
+		const data = model.getData();
+		// find object index in data
+		const objectIndex = data.packages.findIndex((object) => object.name === objectName);
+		if (!objectIndex) {
+			//object not found
+			// return
+		}
+		this.getView().bindElement({
+			path: `/packages/${objectIndex}`,
+			model: "data",
+		});
+	}
+
 	public onNavBack(event: UI5Event): void {
 		const sPreviousHash = History.getInstance().getPreviousHash();
 
