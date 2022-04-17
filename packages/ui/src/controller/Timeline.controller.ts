2efffff6218869b2a04ecfbadd63948db67c6012
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2022-04-17 13:24:30 +0200

    feat(frontend): init timeline with webc

--- packages/ui/src/controller/Timeline.controller.ts
+++ packages/ui/src/controller/Timeline.controller.ts
@@ -4,5 +4,7 @@ import BaseController from "./BaseController";
  * @namespace org.openui5.ui5community.controller
  */
 export default class Timeline extends BaseController {
-	public onInit(): void {}
+	public onInit(): void {
+		console.log("Timeline.onInit");
+	}
 }
