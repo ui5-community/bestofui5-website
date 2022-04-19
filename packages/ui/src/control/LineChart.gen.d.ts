a857db77e8a4c79c50f2fd8913fc264a77aebd63
Peter Muessig <peter.muessig@sap.com>
2022-04-19 10:40:56 +0200

    feat(ui): integrate Chart.js

---
+++ packages/ui/src/control/LineChart.gen.d.ts
@@ -0,0 +1,33 @@
+import { CSSColor } from "sap/ui/core/library";
+import ChartRecord from "org/openui5/ui5community/control/ChartRecord";
+import { $ControlSettings } from "sap/ui/core/Control";
+
+declare module "./LineChart" {
+	/**
+	 * Interface defining the settings object used in constructor calls
+	 */
+	interface $LineChartSettings extends $ControlSettings {
+		title?: string;
+		color?: CSSColor;
+		records?: ChartRecord[] | ChartRecord;
+	}
+
+	export default interface LineChart {
+		// property: title
+		getTitle(): string;
+		setTitle(title: string): this;
+
+		// property: color
+		getColor(): CSSColor;
+		setColor(color: CSSColor): this;
+
+		// aggregation: records
+		getRecords(): ChartRecord[];
+		addRecord(records: ChartRecord): this;
+		insertRecord(records: ChartRecord, index: number): this;
+		removeRecord(records: number | string | ChartRecord): this;
+		removeAllRecords(): ChartRecord[];
+		indexOfRecord(records: ChartRecord): number;
+		destroyRecords(): this;
+	}
+}
--- packages/ui/src/controller/App.controller.ts
+++ packages/ui/src/controller/App.controller.ts
@@ -1,11 +1,38 @@
 import BaseController from "./BaseController";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import formatter from "../model/formatter";
+import Sorter from "sap/ui/model/Sorter";
+import QueryControl from "./QueryControl";
 
 /**
  * @namespace org.openui5.ui5community.controller
  */
 export default class App extends BaseController {
+	private formatter = formatter;
+
 	public onInit(): void {
-		// apply content density mode to root view
-		this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		this.queryControl = new QueryControl(this.getView());
+	}
+
+	public liveSearch(event): void {
+		const value = event.getParameter("value").trim();
+		this.getView().getModel("settings").setProperty("/search", value);
+		if (this.getRouter().getHashChanger().getHash() != "packages") {
+			this.getView().getModel("settings").setProperty("/iconTabHeaderKey", "allPackages");
+			this.navTo("allPackages");
+		} else {
+			this.getView().getParent().byId("AllPackages").getController().onPatternMatched();
+		}
+	}
+
+	public onUpdateToken(event): void {
+		this.queryControl.onUpdateToken(event);
+		if (this.getRouter().getHashChanger().getHash() != "packages") {
+			this.getView().getModel("settings").setProperty("/iconTabHeaderKey", "allPackages");
+			this.navTo("allPackages");
+		} else {
+			this.getView().getParent().byId("AllPackages").getController().onPatternMatched();
+		}
 	}
 }
