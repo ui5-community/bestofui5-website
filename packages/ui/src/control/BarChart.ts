52afab1a3e5aee4aaaa4b272d4d3e025ded3833a
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2022-04-19 18:19:01 +0200

    feat(frontend): show npm downloads history on object page (#120)
    * feat(frontend): show npm downloads history on object page
    
    * feat(frontend): bump ui5-tooling-modules version
    
    * fix(frontend): fix scrollbar issue on objectpagelayout
    
    * feat(fronted): change main to app view

---
+++ packages/ui/src/control/BarChart.ts
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import Control from "sap/ui/core/Control";
+// https://www.chartjs.org/docs/latest/getting-started/integration.html#bundlers-webpack-rollup-etc
+import { Chart, registerables } from "chart.js";
+import RenderManager from "sap/ui/core/RenderManager";
+
+/**
+ * @name org.openui5.ui5community.control.BarChart
+ */
+export default class BarChart extends Control {
+	private _chart: Chart;
+	// The following three lines were generated and should remain as-is to make TypeScript aware of the constructor signatures
+	constructor(idOrSettings?: string | $BarChartSettings);
+	constructor(id?: string, settings?: $BarChartSettings);
+	constructor(id?: string, settings?: $BarChartSettings) {
+		super(id, settings);
+	}
+
+	static readonly metadata = {
+		properties: {
+			title: "string",
+			color: "sap.ui.core.CSSColor",
+		},
+		aggregations: {
+			records: {
+				type: "org.openui5.ui5community.control.ChartRecord",
+			},
+		},
+		defaultAggregation: "records",
+	};
+
+	static renderer = {
+		apiVersion: 2,
+		render: (rm: RenderManager, chart: BarChart) => {
+			rm.openStart("div", chart);
+			rm.style("color", chart.getColor());
+			rm.style("padding", "2em");
+			rm.openEnd();
+
+			rm.openStart("canvas", chart.getId() + "-canvas");
+			rm.openEnd();
+			rm.close("canvas");
+
+			rm.close("div");
+		},
+	};
+
+	_getChartData() {
+		const aRecords = this.getRecords();
+		return {
+			labels: aRecords.map((record) => {
+				return record.getLabel();
+			}),
+			datasets: [
+				{
+					label: this.getTitle(),
+					backgroundColor: this.getColor(),
+					borderColor: this.getColor(),
+					data: aRecords.map((record) => {
+						return record.getValue();
+					}),
+				},
+			],
+		};
+	}
+
+	onAfterRendering() {
+		if (!this._chart) {
+			Chart.register(...registerables);
+			this._chart = new Chart(this.getDomRef("canvas") as HTMLCanvasElement, {
+				type: "bar",
+				data: this._getChartData(),
+				options: {
+					responsive: true,
+					animation: false,
+				},
+			});
+		} else {
+			this._chart.data = this._getChartData();
+			this._chart.update();
+		}
+	}
+}
