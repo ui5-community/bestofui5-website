2cda1407477c3df789c4f7833ff7c3ff0a94fe8f
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2022-04-21 20:54:46 +0200

    fix(ui): set selected header key in timeline (#138)
    * fix(ui): set selected header key in timeline
    
    * fix(ui): set selected header key in timeline

--- packages/ui/src/controller/Timeline.controller.ts
+++ packages/ui/src/controller/Timeline.controller.ts
@@ -5,12 +5,16 @@ import BaseController from "./BaseController";
  */
 export default class Timeline extends BaseController {
 	public onInit(): void {
-		console.log("Timeline.onInit");
+		this.getRouter().getRoute("timeline").attachPatternMatched(this.onPatternMatched, this);
 	}
 
 	public onNameClick(event: sap.ui.base.Event): void {
 		this.navTo("RouteObjectView", {
 			name: event.getSource().getBindingContext("versions").getObject().name,
 		});
+	}
+
+	public onPatternMatched(event): void {
+		this.getView().getModel("settings").setProperty("/iconTabHeaderKey", "timeline");
 	}
 }
