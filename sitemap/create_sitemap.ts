2f52ced5cf54a8b96b216b571a8b09961315863b
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2022-04-30 22:27:40 +0200

    feat(seo): add sitemap

---
+++ sitemap/create_sitemap.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env ts-node-script
+import * as fs from "fs";
+import { SitemapStream } from "sitemap";
+import fetch from "node-fetch";
+
+async function buildSitemap() {
+	// Creates a sitemap object given the input configuration with URLs
+	const sitemap = new SitemapStream({ hostname: "https://bestofui5.org" });
+
+	const res = await fetch("https://raw.githubusercontent.com/ui5-community/bestofui5-data/live-data/data/data.json");
+	const data = await res.json();
+
+	const writeStream = fs.createWriteStream("./src/sitemap.xml");
+	sitemap.pipe(writeStream);
+
+	sitemap.write({ url: "/#/packages" });
+	sitemap.write({ url: "/#/tags" });
+	sitemap.write({ url: "/#/timeline" });
+	for (const item of data.packages) {
+		sitemap.write({ url: `/#/packages/${item.name}` });
+	}
+	sitemap.end();
+}
+void buildSitemap();
