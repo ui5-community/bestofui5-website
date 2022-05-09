31f0bfc90490e6f0cc7dd90bbc0b031ee6f0d3e9
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2022-05-09 15:14:45 +0200

    fix(search): make search/filter work again (#190)
    * fix(search): make search/filter work again
    
    * fix(search): remove delay function

--- src/controller/App.controller.ts
+++ src/controller/App.controller.ts
@@ -14,22 +14,28 @@ export default class App extends BaseController {
 
 	public liveSearch(event: Event): void {
 		const value = event.getParameter("value").trim();
+		this.liveSearchHandler(value);
+	}
+
+	public liveSearchHandler(value: string): void {
+		// const value = event.getParameter("value").trim();
 		this.getView().getModel("settings").setProperty("/search", value);
-		if (this.getRouter().getHashChanger().getHash() != "packages") {
+		if (!this.getRouter().getHashChanger().getHash().startsWith("packages")) {
 			this.getView().getModel("settings").setProperty("/headerKey", "allPackages");
 			this.navTo("allPackages");
 		} else {
-			this.getView().getParent().byId("AllPackages").getController().onPatternMatched();
+			this.getView().getParent().byId("AllPackages").getController().applySearchFilter();
 		}
 	}
 
 	public onUpdateToken(event: Event): void {
 		this.queryUtil.onUpdateToken(event);
-		if (this.getRouter().getHashChanger().getHash() != "packages") {
+		if (!this.getRouter().getHashChanger().getHash().startsWith("packages")) {
 			this.getView().getModel("settings").setProperty("/headerKey", "allPackages");
 			this.navTo("allPackages");
 		} else {
-			this.getView().getParent().byId("AllPackages").getController().onPatternMatched();
+			this.getView().getParent().byId("AllPackages").getController().applySearchFilter();
 		}
 	}
+
 }
--- src/controller/HotPackages.controller.ts
+++ src/controller/HotPackages.controller.ts
@@ -1,20 +1,10 @@
 import AppController from "./App.controller";
 import Event from "sap/ui/base/Event";
-import JSONModel from "sap/ui/model/json/JSONModel";
 
 /**
  * @namespace org.openui5.bestofui5.controller
  */
 export default class HotPackages extends AppController {
-	public onInit(): void {
-		this.getRouter().getRoute("default").attachPatternMatched(this.onPatternMatched, this);
-	}
-
-	public onPatternMatched(event: Event): void {
-		(this.getView().getModel("settings") as JSONModel).setProperty("/tokens", []);
-		(this.getView().getModel("settings") as JSONModel).setProperty("/search", "");
-	}
-
 	public onPress(event: Event): void {
 		// get object name from oevent
 		const objectName = event.getSource().getBindingContext("data").getObject().name;
