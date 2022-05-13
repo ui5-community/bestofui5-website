3dacab8d73f2323ad37c138fb183a6086b2d9e02
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2022-05-13 11:12:31 +0200

    feat(ui): add 'not found' pages (#200)

---
+++ src/controller/NotFound.controller.ts
@@ -0,0 +1,10 @@
+import AppController from "./App.controller";
+
+/**
+ * @namespace org.openui5.bestofui5.controller
+ */
+export default class NotFound extends AppController {
+	public onInit(): void {
+		console.log("NotFound.onInit");
+	}
+}
---
+++ src/controller/ObjectNotFound.controller.ts
@@ -0,0 +1,10 @@
+import AppController from "./App.controller";
+
+/**
+ * @namespace org.openui5.bestofui5.controller
+ */
+export default class ObjectNotFound extends AppController {
+	public onInit(): void {
+		console.log("NotFound.onInit");
+	}
+}
