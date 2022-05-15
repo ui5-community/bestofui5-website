e9ea1cc4bc0df972c732ded77d69fb710a31c5ba
Marian Zeis <13335743+marianfoo@users.noreply.github.com>
2022-05-15 14:23:14 +0200

    fix: use urlhelper for opening links (#206)

--- src/controller/Contributors.controller.ts
+++ src/controller/Contributors.controller.ts
@@ -10,10 +10,6 @@ export default class Timeline extends BaseController {
 		this.getRouter().getRoute("contributors").attachPatternMatched(this.onPatternMatched, this);
 	}
 
-	public navToGithubUser(event: Event): void {
-		window.open(event.getSource().getBindingContext("contributors").getObject().url, "_blank");
-	}
-
 	public onPatternMatched(event: Event): void {
 		(this.getView().getModel("settings") as JSONModel).setProperty("/headerKey", "contributors");
 	}
