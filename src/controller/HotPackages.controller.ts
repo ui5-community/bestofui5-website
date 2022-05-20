833b179cf40f8138b61bafd00977543ef9f25495
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-05-20 07:32:26 +0200

    fix: set header button on "Hot Packges" view

--- src/controller/HotPackages.controller.ts
+++ src/controller/HotPackages.controller.ts
@@ -5,6 +5,13 @@ import Event from "sap/ui/base/Event";
  * @namespace org.openui5.bestofui5.controller
  */
 export default class HotPackages extends AppController {
+	public onInit(): void {
+		this.getRouter().getRoute("default").attachPatternMatched(this.onPatternMatched, this);
+	}
+
+	public onPatternMatched(event: Event): void {
+		(this.getView().getModel("settings") as JSONModel).setProperty("/headerKey", "hotPackages");
+	}
 	public onPress(event: Event): void {
 		// get object name from oevent
 		const objectName = event.getSource().getBindingContext("data").getObject().name;
